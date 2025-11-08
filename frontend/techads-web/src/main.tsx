2933f300359f3850e0d0d478dee63d662b0dc101
demolitionl <ph4oalencar@gmail.com>
2025-11-08 17:07:03 -0300

    FIX

--- frontend/techads-web/src/main.tsx
+++ frontend/techads-web/src/main.tsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-async function enableMocking() {
-}
+async function enableMocking() {}
 
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
